859e71d4e6aa8d85600051dbbfd14f1fad526c98
MananTaneja <manantaneja007@gmail.com>
2020-05-30 14:29:02 +0530

    updated toplogical sort

--- Structures/Graph/topologicalSort.cpp
+++ Structures/Graph/topologicalSort.cpp
@@ -7,36 +7,18 @@ void addEdge(vector<int> adj[], int u, int v) {
     adj[u].push_back(v);
 }
 
-void topologicalSortUtil(int current, vector<bool>& visited, stack<int>& order, vector<int> adj[], int N) {
+void topologicalSort(int current, vector<bool>& visited, stack<int>& order, vector<int> adj[], int N) {
     
     visited[current] = true;
     for(int x: adj[current]) {
         if(!visited[x]) {
-            topologicalSortUtil(x, visited, order, adj, N);
+            topologicalSort(x, visited, order, adj, N);
         }
     }
 
     order.push(current);
 }
 
-void topologicalSort(vector<int> adj[], int N) {
-    // Data Structures required: set and stack
-    vector<bool> visited(N, false);
-    stack<int> order;
-
-    for(int i = 0; i < N; i++) {
-        if(!visited[i]) {
-            topologicalSortUtil(i, visited, order, adj, N);
-        }
-    } 
-
-    cout << "Topological Sort: ";
-    while(!order.empty()) {
-        cout << order.top() << ", ";
-        order.pop();
-    }
-    cout << endl;
-}
 
 
 int main() {
@@ -49,7 +31,22 @@ int main() {
     addEdge(adj, 2, 3); 
     addEdge(adj, 3, 1); 
     
-    topologicalSort(adj, N);
+     // Data Structures required: set and stack
+    vector<bool> visited(N, false);
+    stack<int> order;
+
+    for(int i = 0; i < N; i++) {
+        if(!visited[i]) {
+            topologicalSort(i, visited, order, adj, N);
+        }
+    } 
+
+    cout << "Topological Sort: ";
+    while(!order.empty()) {
+        cout << order.top() << ", ";
+        order.pop();
+    }
+    cout << endl;
 
     return 0;
 }
